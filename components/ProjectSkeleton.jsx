9813d24ba6ef27effec6724c67971aa0ec3af436
Ritik01417 <ritikkamwal797@gmail.com>
2025-07-10 00:23:43 +0530

    Update in skeleton section

--- components/ProjectSkeleton.jsx
+++ components/ProjectSkeleton.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './projectSkeleton.css';
 
-const ProjectSkeleton = () => {
+export default function ProjectSkeleton() {
   return (
-    <div className="project-skeleton-card">
+    <div className="skeleton-card">
       <div className="skeleton-header">
-        <div className="skeleton-title shimmer"></div>
-        <div className="skeleton-button shimmer"></div>
+        <div className="skeleton-title shimmer" />
+        <div className="skeleton-button shimmer" />
       </div>
-      <div className="skeleton-desc shimmer"></div>
-      
+      <div className="skeleton-description shimmer" />
     </div>
   );
-};
+}
 
-export default ProjectSkeleton;
